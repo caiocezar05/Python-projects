a0c92c68f6cc32589d94b9c1ec526fc7ef3b2548
caiocezar05 <54714305+caiocezar05@users.noreply.github.com>
2019-10-14 01:49:37 -0300

    Add files via upload

--- contract_reader.py
+++ contract_reader.py
@@ -7,27 +7,79 @@ import pytesseract
 from wand.image import Image as wi
 import fnmatch
 import re
+import shutil
+from sklearn.externals import joblib
 
-pdfpath = r"C:\Users\caio.santos\Desktop\teste\teste construdecor"
-docspath = r"C:\Users\caio.santos\Desktop\teste\docs"
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\caio.santos\AppData\Local\Tesseract-OCR\tesseract.exe"
 
-def get_pdf_text(pdf_path, out):
+pdfpath = r"C:\Users\caio.santos\Desktop\test constr"
+docspath = r"C:\Users\caio.santos\Desktop\teste\docs"
+
+
+def get_files_fromdirectory(in_files, out_files):
+    for root, dirs, files in os.walk(in_files):  # replace the . with your starting directory
+        for file in files:
+            path_file = os.path.join(root, file)
+            shutil.copy2(path_file, out_files)  # change you destination dir
+
+
+def clause_split(data):
+    docnum = []
+    clausula = []
+    text = []
+
+    rex = re.compile(r"cl[á,a]usula", flags=re.I)
+    # rex = re.compile(r"(PARÁGRAFO\s\d+?º)|(\w+?:\d+?\.\d+?\.)")
+    ndoc = 0
+
+    if type(data) == str:
+        cl = 0
+        ndoc += 1
+        for string in re.split(rex, data):
+            cl += 1
+            docnum.append(ndoc)
+            clausula.append(cl - 1)
+            text.append(string)
+
+    else:
+        for d in data:
+            ndoc += 1
+            cl = 0
+
+            for string in re.split(rex, d):
+                cl += 1
+                docnum.append(ndoc)
+                clausula.append(cl - 1)
+                text.append(string)
+    data = {'Doc': docnum, 'clausula': clausula, 'Text': text}
+    df = pd.DataFrame(data=data)
+    return df
+
+
+def pdf2docx_convert(pdf_path, out):
     doc = docx.Document()
-    pdf=wi(filename=pdf_path,resolution=300)
-    pdfImg=pdf.convert('jpeg')
-    imgBlobs=[]
+    text = get_pdf_text(pdf_path)
+    doc.add_paragraph(text)
+    doc.save(out + '.docx')
+
+
+def get_pdf_text(pdf_path):
+    pdf = wi(filename=pdf_path, resolution=300)
+    pdfImg = pdf.convert('jpeg')
+    imgBlobs = []
+    text = []
 
     for img in pdfImg.sequence:
-        page=wi(image=img)
+        page = wi(image=img)
         imgBlobs.append(page.make_blob('jpeg'))
 
     for imgBlob in imgBlobs:
-        im=Image.open(io.BytesIO(imgBlob))
-        text=pytesseract.image_to_string(im,lang='por')
-        doc.add_paragraph(text)
+        im = Image.open(io.BytesIO(imgBlob))
+        tx = pytesseract.image_to_string(im, lang='por')
+        text.append(tx)
 
-    doc.save(out +'.docx')
+    return text
+
 
 def trainprepare(path):
     os.chdir(path)
@@ -36,7 +88,7 @@ def trainprepare(path):
     docnum = []
     clausula = []
     text = []
-    rex = re.compile(r"CLÁUSULA")
+    rex = re.compile(r"cl[á,a]usula", flags=re.I)
     for d in fnmatch.filter(os.listdir(), '*.docx'):
         doc = docx.Document(d)
         ndoc += 1
@@ -48,44 +100,148 @@ def trainprepare(path):
         for string in re.split(rex, ''.join(stringx)):
             cl += 1
             docnum.append(ndoc)
-            clausula.append(cl-1)
+            clausula.append(cl - 1)
             text.append(string)
 
     data = {'Doc': docnum, 'clausula': clausula, 'Text': text}
     df = pd.DataFrame(data=data)
     return df
 
-def trainprepareDF(dataf):
+
+def trainprepare_DF_series(data):
     docnum = []
     clausula = []
     text = []
-    # PARÁGRAFO 1º
-    # OBJETO:1.1.
-    # LOCAÇÃO:2.1.
-    # PRAZO4.
-    # rex = re.compile(r"PARÁGRAFO\s\d+?º")
-    rex = re.compile(r"(PARÁGRAFO\s\d+?º)|(\w+?:\d+?\.\d+?\.)")
 
-    for i in range(0, 21):
-        ndoc = dataf.iloc[i][0]
+    rex = re.compile(r"cl[á,a]usula", flags=re.I)
+    # rex = re.compile(r"(par[á,a]grafo\s\d+?º)|(\w+?:\d+?\.\d+?\.)")
+    ndoc = 0
+    for d in data:
+        ndoc += 1
         cl = 0
-        stringx = dataf.iloc[i][2]
 
-        for string in re.split(rex, stringx):
+        for string in re.split(rex, d):
             cl += 1
             docnum.append(ndoc)
             clausula.append(cl - 1)
             text.append(string)
 
     data = {'Doc': docnum, 'clausula': clausula, 'Text': text}
-    dff = pd.DataFrame(data=data)
-    return dff
+    df = pd.DataFrame(data=data)
+    return df
 
 
-trainprepare(docspath).to_excel('test contracts.xlsx')
+def get_patter(df):
+    text = df[0]
+    cls = df[1]
+
+    if cls == 'OBJ':
+        string = []
+        rex = re.compile(r"objeto...+\.")
+        rex2 = re.compile(r"\n\n..+\.")
+        for i in rex.findall(text):
+            string.append(i)
+        for i in rex2.findall(text):
+            string.append(i)
+        strin = '; '.join(string)
+
+        return strin
+
+    elif cls == 'VALOR':
+        string = []
+        rex = re.compile(r"R\$\s?\d.+?\s")
+        for i in rex.findall(text):
+            string.append(i)
+        strin = '; '.join(string)
+
+        return strin
+
+    elif cls == 'MULTA':
+        string = []
+        rex = re.compile(r"\d+?\%.+\.")
+        rex2 = re.compile(r"R\$\s?\d.+?\s")
+
+        for i in rex.findall(text):
+            string.append(i)
+
+        for i in rex2.findall(text):
+            string.append('; ')
+            string.append(i)
+
+        strin = ''.join(string)
+        return strin
 
 
+    elif cls == 'PRAZO':
+        string = []
+        rex = re.compile(r"\d\d\s?\(.+\)\s?\w+")
+        rex2 = re.compile(r"\d\d\sde\s\w+\sde\s\d\d\d\d")
+        rex3 = re.compile(r"\d\d\/\d\d\/\d\d\d\d")
+
+        for i in rex.findall(text):
+            string.append(i)
+
+        for i in rex2.findall(text):
+            string.append('; ')
+            string.append(i)
+
+        for i in rex3.findall(text):
+            string.append('; ')
+            string.append(i)
+
+        strin = ''.join(string)
+        return strin
+
+    else:
+        return "nothing"
 
 
+def contract_reader_from_file(file):
+    # model_adit_or_master = joblib.load('Master_aditiv_classifier')
+    model_clause = joblib.load('Clause_classifier')
 
+    if file.lower().endswith(('.pdf', '.PDF')):
+        string = get_pdf_text(file)
+        text = ''.join(string)
+
+
+    elif file.endswith('.docx'):
+        doc = docx.Document(file)
+        string = []
+        for p in doc.paragraphs:
+            string.append(p.text)
+        text = ''.join(string)
+
+    df = clause_split(text)
+    cl_class = model_clause.predict(df['Text'])
+    # contr_type = model_adit_or_master.predict(text)
+
+    # df['Contr Type'] = contr_type
+    df['Cl class'] = cl_class
+
+    df['Info'] = df[['Text', 'Cl class']].apply(get_patter, axis=1)
+    return df
+
+
+def contract_reader(data):
+    model_clause = joblib.load('Clause_classifier')
+    # model_adit_or_master = joblib.load('Master_aditiv_classifier')
+
+    # contr_type = model_adit_or_master.predict(data)
+
+    df = clause_split(data)
+    cl_class = model_clause.predict(df['Text'])
+    df['Cl class'] = cl_class
+    # df['Contr Type'] = contr_type
+    df['Info'] = df[['Text', 'Cl class']].apply(get_patter, axis=1)
+    return df
+
+os.chdir(r'C:\Users\caio.santos\Desktop\Allpark')
+n=46
+
+for f in fnmatch.filter(os.listdir(), '*.pdf')[n:]:
+    n+=1
+    pdf2docx_convert(f,f'{str(n)} - {f}')
+    print(f)
+    print(n)
 
